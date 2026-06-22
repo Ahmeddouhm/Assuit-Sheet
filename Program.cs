c35f290b783bade69937c474fc43105d3d19aa77
Ahmeddouhm <a.douha0@gmail.com>
2026-06-22 07:47:28 +0300

    Add "H - Two numbers"

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
-﻿long n = long.Parse(Console.ReadLine());
+﻿string[] input = Console.ReadLine().Split();
 
-Console.WriteLine((n*(n+1))/2);
+decimal a = decimal.Parse(input[0]);
+decimal b = decimal.Parse(input[1]);
+decimal result = a / b;
+
+Console.WriteLine($"floor {a} / {b} = {Math.Floor(result)}");
+Console.WriteLine($"ceil {a} / {b} = {Math.Ceiling(result)}");
+Console.WriteLine($"round {a} / {b} = {Math.Round(result,MidpointRounding.AwayFromZero)}");
