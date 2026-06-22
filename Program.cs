b8206e0ac14765d08830c049380d63f3a5109a1e
Ahmeddouhm <a.douha0@gmail.com>
2026-06-22 07:13:59 +0300

    Add "F - Digits Summation"

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿const double pi = Math.PI;
-double r = double.Parse(Console.ReadLine());
+﻿string[] input = Console.ReadLine().Split();
 
-Console.WriteLine(pi * Math.Pow(r,2));
+int lastNum1 = int.Parse(input[0].Substring(input[0].Length-1));
+int lastNum2 = int.Parse(input[1].Substring(input[1].Length-1));
+
+Console.WriteLine(lastNum1+lastNum2);
