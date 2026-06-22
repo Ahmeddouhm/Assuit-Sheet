55e27f9fb24a9eda01cb229bb95f25e5ad973648
Ahmeddouhm <a.douha0@gmail.com>
2026-06-22 03:48:18 +0300

    Add "B - Basic Data Types"

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine($"Hello, {Console.ReadLine()}");
+﻿string[] input = Console.ReadLine().Split();
+
+// int long char float double 
+
+Console.WriteLine(int.Parse(input[0]));
+Console.WriteLine(long.Parse(input[1]));
+Console.WriteLine(char.Parse(input[2]));
+Console.WriteLine(float.Parse(input[3]));
+Console.WriteLine(double.Parse(input[4]));
