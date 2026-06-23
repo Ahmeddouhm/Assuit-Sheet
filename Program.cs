d9c669da364cae3c1027b074d846c110efdd4a0a
Ahmeddouhm <a.douha0@gmail.com>
2026-06-24 02:12:42 +0300

    Add "S - Interval"

--- Program.cs
+++ Program.cs
@@ -1,11 +1,25 @@
-﻿long ageInDays = long.Parse(Console.ReadLine());
-long days, months, years , livedYears;
+﻿double num = double.Parse(Console.ReadLine());
 
-years = ageInDays / 365;
-livedYears = ageInDays - years * 365;
-months = livedYears / 30;
-days = livedYears - (months * 30);
+// [0,25], (25,50], (50,75], (75,100]
+// Out Of Intervals
 
-Console.WriteLine($"{years} years");
-Console.WriteLine($"{months} months");
-Console.WriteLine($"{days} days");
+if (num < 0 || num > 100)
+{
+    Console.WriteLine("Out of Intervals");
+}
+else if (num >= 0 && num <= 25)
+{
+    Console.WriteLine("Interval [0,25]");
+}
+else if (num > 25 && num <= 50)
+{
+    Console.WriteLine("Interval (25,50]");
+}
+else if (num > 50 && num <= 75)
+{
+    Console.WriteLine("Interval (50,75]");
+}
+else if (num > 75 && num <= 100)
+{
+    Console.WriteLine("Interval (75,100]");
+}
