6eea62660262a3dcf50375ad758440dbc01b46da
Ahmeddouhm <a.douha0@gmail.com>
2026-06-23 08:16:08 +0300

    Add "Q - Coordinates of a Point"

--- Program.cs
+++ Program.cs
@@ -1,3 +1,32 @@
-﻿string num = Console.ReadLine();
-int firstDigit = int.Parse(num[0].ToString());
-Console.WriteLine(int.IsEvenInteger(firstDigit) ? "EVEN" : "ODD");
+﻿string[] points = Console.ReadLine().Split();
+double x = double.Parse(points[0]);
+double y = double.Parse(points[1]);
+
+if (x == 0 && y == 0)
+{
+    Console.WriteLine("Origem");
+}
+else if (x == 0)
+{
+    Console.WriteLine("Eixo Y");
+}
+else if (y == 0)
+{
+    Console.WriteLine("Eixo X");
+}
+else if (double.IsPositive(x) && double.IsPositive(y))
+{
+    Console.WriteLine("Q1");
+}
+else if (double.IsNegative(x) && double.IsPositive(y))
+{
+    Console.WriteLine("Q2");
+}
+else if (double.IsNegative(x) && double.IsNegative(y))
+{
+    Console.WriteLine("Q3");
+}
+else
+{
+    Console.WriteLine("Q4");
+}
