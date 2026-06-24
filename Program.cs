913044829de4f4eaed75c01f18079e76134e1dae
Ahmeddouhm <a.douha0@gmail.com>
2026-06-24 05:02:55 +0300

    Add "W - Mathematical Expression"

--- Program.cs
+++ Program.cs
@@ -1,20 +1,26 @@
 ﻿string input = Console.ReadLine();
 string[] stringNums = input.Split();
+
 int a = int.Parse(stringNums[0]);
 int b = int.Parse(stringNums[2]);
+int c = int.Parse(stringNums[4]);
+
 char opeartor = char.Parse(stringNums[1]);
 
-if (opeartor == '<')
+if (opeartor == '+')
 {
-    Console.WriteLine(a < b ? "Right" : "Wrong");
+    int sum = a + b;
+    Console.WriteLine((sum == c) ? "Yes" : sum);
 }
-else if (opeartor == '=')
+else if (opeartor == '-')
 {
-    Console.WriteLine(a == b ? "Right" : "Wrong");
+    int diff = a - b;
+    Console.WriteLine(diff == c ? "Yes" : diff);
 
 }
-else if (opeartor == '>')
+else if (opeartor == '*')
 {
-    Console.WriteLine(a > b ? "Right" : "Wrong");
+    int mtply = a * b;
+    Console.WriteLine(mtply == c ? "Yes" : mtply);
 
 }
