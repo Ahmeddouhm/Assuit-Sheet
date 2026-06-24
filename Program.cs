34698eff8bb3dd1c8f945a62d3e11fc4a7cef462
Ahmeddouhm <a.douha0@gmail.com>
2026-06-24 20:25:20 +0300

    Add "B - Even Numbers"

--- Program.cs
+++ Program.cs
@@ -1,6 +1,14 @@
 ﻿int num = int.Parse(Console.ReadLine());
 
+if (num <= 1)
+{
+    Console.WriteLine("-1");
+}
+
 for (int i = 1; i <= num; i++)
 {
-    Console.WriteLine(i);
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
 }
