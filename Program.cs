664cc1056d260c5cd65696ceba69e34a21e658e3
Ahmeddouhm <a.douha0@gmail.com>
2026-06-24 07:08:56 +0300

    Add "Z - Hard Compare"

--- Program.cs
+++ Program.cs
@@ -1,10 +1,18 @@
 ﻿string[] stringNums = Console.ReadLine().Split();
 
-Int128 a = Int128.Parse(stringNums[0]);
-Int128 b = Int128.Parse(stringNums[1]);
-Int128 c = Int128.Parse(stringNums[2]);
-Int128 d = Int128.Parse(stringNums[3]);
+double a = double.Parse(stringNums[0]);
+double b = double.Parse(stringNums[1]);
+double c = double.Parse(stringNums[2]);
+double d = double.Parse(stringNums[3]);
 
-string strResult = (a * b * c * d).ToString();
+double leftSide = b * Math.Log(a);
+double rightSide = d * Math.Log(c);
 
-Console.WriteLine($"{strResult[^2..]}");
+if (leftSide > rightSide)
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+}
