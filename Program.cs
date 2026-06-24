7045f2d30703c2916ed6786178f754dcfcfd7894
Ahmeddouhm <a.douha0@gmail.com>
2026-06-24 20:13:39 +0300

    Add "A - 1 to N"

--- Program.cs
+++ Program.cs
@@ -1,18 +1,1 @@
-﻿string[] stringNums = Console.ReadLine().Split();
-
-double a = double.Parse(stringNums[0]);
-double b = double.Parse(stringNums[1]);
-double c = double.Parse(stringNums[2]);
-double d = double.Parse(stringNums[3]);
-
-double leftSide = b * Math.Log(a);
-double rightSide = d * Math.Log(c);
-
-if (leftSide > rightSide)
-{
-    Console.WriteLine("YES");
-}
-else
-{
-    Console.WriteLine("NO");
-}
+﻿
