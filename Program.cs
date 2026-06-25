639b2037438650486b486979dc5247f0db2ef031
Ahmeddouhm <a.douha0@gmail.com>
2026-06-25 22:07:56 +0300

    Add "C - Even, Odd, Positive and Negative"

--- Program.cs
+++ Program.cs
@@ -1,14 +1,28 @@
-﻿int num = int.Parse(Console.ReadLine());
+﻿int itr = int.Parse(Console.ReadLine());
+string[] values = Console.ReadLine().Split();
+int positives = 0, negatives = 0, evens = 0, odds = 0;
 
-if (num <= 1)
+for (int i = 0; i < itr; i++)
 {
-    Console.WriteLine("-1");
-}
+	int num = int.Parse(values[i]);
 
-for (int i = 1; i <= num; i++)
-{
-    if (i % 2 == 0)
+	if (num % 2 == 0)
+	{
+		evens++;
+	}
+    else if (num % 2 != 0)
     {
-        Console.WriteLine(i);
-    }
+		odds++;
+	}
+
+	if (num > 0)
+	{
+		positives++;
+	}
+	else if (num < 0)
+	{
+		negatives++;
+	}
 }
+
+Console.WriteLine($"Even: {evens}\r\nOdd: {odds}\r\nPositive: {positives}\r\nNegative: {negatives}");
