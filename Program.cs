4afbf8f682b93e6760a471f3da4ea21f02cce150
Ahmeddouhm <a.douha0@gmail.com>
2026-07-02 04:48:28 +0300

    Add "K - Divisors" OPTIMIZED

--- Program.cs
+++ Program.cs
@@ -1,8 +1,30 @@
-﻿int num = int.Parse(Console.ReadLine());
+﻿// Optimized Divisors O(sqrt(n))
 
-for (int i = 1; i <= num; i++)
+int num = int.Parse(Console.ReadLine());
+
+List<int> res = new();
+List<int> larges = new();
+
+for (int i = 1; i*i <= num; i++)
 {
-    if (num % i == 0)
-        Console.WriteLine(i);
+    if (num % i == 0) 
+    {
+        if (i == num / i)
+        {
+            res.Add(i);
+        }
+        else
+        {
+            res.Add(i);
+            larges.Add(num / i);
+        }
+    }
+}
 
+res.AddRange(larges);
+res.Sort();
+
+foreach (var item in res)
+{
+    Console.WriteLine(item);
 }
