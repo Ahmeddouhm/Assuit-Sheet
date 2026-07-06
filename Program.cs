b2003a58268e9b6c1fbd2afb2600cadbee88b860
Ahmeddouhm <a.douha0@gmail.com>
2026-07-06 16:47:08 +0300

    Add "P - Shape1"

--- Program.cs
+++ Program.cs
@@ -1,35 +1,10 @@
-﻿using System.Text;
+﻿int itr = int.Parse(Console.ReadLine());
 
-string[] inputNums = Console.ReadLine().Split();
-long start = long.Parse(inputNums[0]);
-long end = long.Parse(inputNums[1]);
-StringBuilder luckyNums = new();
-
-for (long i = start; i <= end; i++)
+for (int j = itr; j > 0; j--)
 {
-	string currNum = i.ToString();
-
-	for (int j = 0; j < currNum.Length; j++)
-	{
-		if (currNum[j] != '7' && currNum[j] != '4')
-		{
-			break;
-		}
-		else
-		{
-			if (j == currNum.Length - 1)
-			{
-				luckyNums.Append(i+" ");
-			}
-		}
-	}
-}
-
-if (luckyNums.Length == 0)
-{
-    Console.WriteLine("-1");
-}
-else
-{
-    Console.WriteLine($"{luckyNums}");
+    for (int i = j; i > 0; i--)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
 }
