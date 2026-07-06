6d5d81baf40e45d114bdab0f36222cb2b166972e
Ahmeddouhm <a.douha0@gmail.com>
2026-07-06 17:58:41 +0300

    Add "S - Sum of Consecutive Odd Numbers"

--- Program.cs
+++ Program.cs
@@ -1,28 +1,26 @@
-﻿using System.Text;
+﻿int num = int.Parse(Console.ReadLine());
 
-while (true)
+
+for (int i = 0; i < num; i++)
 {
     string[] inputNums = Console.ReadLine().Split();
 
     int num1 = int.Parse(inputNums[0]);
     int num2 = int.Parse(inputNums[1]);
-    StringBuilder numsInBetween = new();
 
-    if (num1 <= 0 || num2 <= 0)
-        break;
-
-    int start = Math.Min(num1,num2);
-    int end = Math.Max(num1,num2);
-
-    for (int i = start; i <= end; i++)
-    {
-        numsInBetween.Append(i+" ");
-    }
+    int start = Math.Min(num1, num2);
+    int end = Math.Max(num1, num2);
 
     int sum = 0;
 
-    for (int i = start; i <= end; i++)
-        sum += i;
+    for (int j = start + 1; j < end; j++)
+    {
+        if (j % 2 != 0)
+        {
+            sum += j;
+        }
 
-    Console.WriteLine($"{numsInBetween.ToString()}sum ={sum}");
+    }
+
+    Console.WriteLine($"{sum}");
 }
