99c40a63dfe123e43d8081c3ef7cb4b2ed834e60
Ahmeddouhm <a.douha0@gmail.com>
2026-07-08 18:41:42 +0300

    Add "V - PUM"

--- Program.cs
+++ Program.cs
@@ -1,33 +1,9 @@
-﻿string[] inputNums = Console.ReadLine().Split();
+﻿int n = int.Parse(Console.ReadLine());
+int itr = 1;
 
-int n = int.Parse(inputNums[0]);
-int a = int.Parse(inputNums[1]);
-int b = int.Parse(inputNums[2]);
-int sum = 0;
-
-for (int i = a; i <= n; i++)
+for (int i = 0; i < n; i++)
 {
-    int sumOfDigits = SumOfDigits(i);
-
-    if (sumOfDigits >= a && sumOfDigits <= b)
-    {
-        sum += i;
-    } 
-}
-
-Console.WriteLine(sum);
-
-static int SumOfDigits(int num) 
-{
-    string strNum = num.ToString();
-    int sum = 0;
-
-    for (int i = 0; i < strNum.Length; i++)
-    {
-        int lastDigit = num % 10;
-        num /= 10;
-        sum += lastDigit;
-    }
-
-    return sum;
+    Console.Write($"{itr} {itr + 1} {itr + 2} PUM");
+    Console.WriteLine();
+    itr += 4;
 }
