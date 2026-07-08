f8b3b7cd885f175f1bc617737827e57f61201417
Ahmeddouhm <a.douha0@gmail.com>
2026-07-08 18:45:47 +0300

    Add "W - Shape3"

--- Program.cs
+++ Program.cs
@@ -1,9 +1,31 @@
-﻿int n = int.Parse(Console.ReadLine());
-int itr = 1;
+﻿int num = int.Parse(Console.ReadLine());
 
-for (int i = 0; i < n; i++)
+for (int i = 1; i <= num; i++)
 {
-    Console.Write($"{itr} {itr + 1} {itr + 2} PUM");
+    for (int j = num; j > i; j--)
+    {
+        Console.Write(" ");
+    }
+
+    for (int k = 1; k <= 2 * i - 1; k++)
+    {
+        Console.Write("*");
+    }
+
     Console.WriteLine();
-    itr += 4;
+}
+
+for (int i = num; i >= 1; i--)
+{
+    for (int j = i; j < num; j++)
+    {
+        Console.Write(" ");
+    }
+
+    for (int k = 2 * i - 1; k >= 1; k--)
+    {
+        Console.Write("*");
+    }
+
+    Console.WriteLine();
 }
