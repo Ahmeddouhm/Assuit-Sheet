fb642f69982e79a1dc5e9db307fae7af76a5d105
Ahmeddouhm <a.douha0@gmail.com>
2026-07-09 20:00:29 +0300

    Add "A - Summation"

--- Program.cs
+++ Program.cs
@@ -1,13 +1,11 @@
-﻿string[] nums = Console.ReadLine().Split();
-int k = int.Parse(nums[0]), s = int.Parse(nums[1]), counter = 0;
+﻿int itr = int.Parse(Console.ReadLine());
+string[] nums = Console.ReadLine().Split();
+long summation = 0;
 
-for (int i = 0; i <= k; i++)
+for (int i = 0; i < itr; i++)
 {
-	for (int j = 0; j <= k; j++)
-	{
-		if (s - i - j >= 0 && s - i - j <= k)
-			counter++;
-	}
+    long currNum = long.Parse(nums[i]);
+    summation += currNum;
 }
 
-Console.WriteLine(counter);
+Console.WriteLine(Math.Abs(summation));
