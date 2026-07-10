dd8ad639d9351b3d9ffab03e1c3dc0a565ab125d
Ahmeddouhm <a.douha0@gmail.com>
2026-07-10 15:50:57 +0300

    Add "E - Lowest Number"

--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
 ﻿int itr = int.Parse(Console.ReadLine());
 string[] nums = Console.ReadLine().Split();
+int min = int.MaxValue , idx = int.MinValue;
 
 for (int i = 0; i < itr; i++)
 {
     int currNum = int.Parse(nums[i]);
 
-	if (currNum <= 10)
+	if (currNum < min)
 	{
-        Console.WriteLine($"A[{i}] = {currNum}");
+		min = currNum;
+		idx = i + 1;
 	}
 }
+
+Console.WriteLine($"{min} {idx}");
