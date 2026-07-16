cf3e0654e1a1deeb552f429218225a42ef7015a7
Ahmeddouhm <a.douha0@gmail.com>
2026-07-16 12:26:59 +0300

    Add "K - Sum Digits"

--- Program.cs
+++ Program.cs
@@ -1,27 +1,10 @@
 ﻿int itr = int.Parse(Console.ReadLine());
-string[] nums = Console.ReadLine().Split();
-long[] arr = new long[itr];
+string nums = Console.ReadLine();
+long sum = 0;
 
-for (long i = 0; i < itr; i++)
+for (int i = 0; i < itr; i++)
 {
-    arr[i] = long.Parse(nums[i]);
+    sum += long.Parse(nums[i].ToString());
 }
 
-long min = arr[0];
-int minFreq = 1;
-
-
-for (int i = 1; i < itr; i++)
-{
-    if (arr[i] < min)
-    {
-        min = arr[i];
-        minFreq = 1;
-    }
-    else if (arr[i] == min)
-    {
-        minFreq++;
-    }
-}
-
-Console.WriteLine(minFreq % 2 == 0 ? "Unlucky" : "Lucky");
+Console.WriteLine(sum);
