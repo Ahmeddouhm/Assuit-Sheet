4179c62856e94f9bce4efeb234ef2f2e60134f50
Ahmeddouhm <a.douha0@gmail.com>
2026-07-16 14:40:31 +0300

    Add "L - Max Subarray"

--- Program.cs
+++ Program.cs
@@ -1,10 +1,26 @@
-﻿int itr = int.Parse(Console.ReadLine());
-string nums = Console.ReadLine();
-long sum = 0;
+﻿int cases = int.Parse(Console.ReadLine());
 
-for (int i = 0; i < itr; i++)
+while (cases > 0)
 {
-    sum += long.Parse(nums[i].ToString());
-}
+    int itr = int.Parse(Console.ReadLine());
+    string[] numsInput = Console.ReadLine().Split();
+    int[] arr = Array.ConvertAll(numsInput , int.Parse);
 
-Console.WriteLine(sum);
+    for (int i = 0; i < itr; i++)
+    {
+        int currMax = arr[i];
+
+        for (int j = i; j < itr; j++)
+        {
+            if (arr[j] > currMax)
+            {
+                currMax = arr[j];
+            }
+
+            Console.Write($"{currMax} ");
+        }
+    }
+    Console.WriteLine();
+
+    cases--;
+}
