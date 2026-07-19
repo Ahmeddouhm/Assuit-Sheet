17c58bc6f3f63d3bc043818eef702b954860bf34
Ahmeddouhm <a.douha0@gmail.com>
2026-07-19 14:52:49 +0300

    Add "N-Check Code"

--- Program.cs
+++ Program.cs
@@ -1,26 +1,57 @@
-﻿int cases = int.Parse(Console.ReadLine());
+﻿string[] numsInput = Console.ReadLine().Split();
+int a = int.Parse(numsInput[0]);
+int b = int.Parse(numsInput[1]);
+string input = Console.ReadLine();
 
-while (cases > 0)
+//try
+//{
+//    bool checkLength = input.Length == a + b + 1;
+//    bool checkDash = input[a] == '-' && input.Count('-') == 1;
+//    bool checkDigits = true;
+//    string[] digitsInput = input.Split('-');
+//    string pt1 = digitsInput[0];
+//    string pt2 = digitsInput[1];
+
+//    for (int i = 0; i < pt1.Length; i++)
+//    {
+//        if (!char.IsDigit(pt1[i]))
+//        {
+//            checkDigits = false;
+//            break;
+//        }
+//    }
+//    for (int i = 0; i < pt2.Length; i++)
+//    {
+//        if (!char.IsDigit(pt2[i]))
+//        {
+//            checkDigits = false;
+//            break;
+//        }
+//    }
+
+//    Console.WriteLine(checkDigits && checkDash && checkLength ? "Yes" : "No");
+//}
+//catch
+//{
+//    Console.WriteLine("No");
+//}
+
+if (input[a] != '-')
 {
-    int itr = int.Parse(Console.ReadLine());
-    string[] numsInput = Console.ReadLine().Split();
-    int[] arr = Array.ConvertAll(numsInput , int.Parse);
-
-    for (int i = 0; i < itr; i++)
-    {
-        int currMax = arr[i];
-
-        for (int j = i; j < itr; j++)
-        {
-            if (arr[j] > currMax)
-            {
-                currMax = arr[j];
-            }
-
-            Console.Write($"{currMax} ");
-        }
-    }
-    Console.WriteLine();
-
-    cases--;
+    Console.WriteLine("No");
+    return;
 }
+
+for (int i = 0; i < input.Length; i++)
+{
+    if (char.IsDigit(input[i]))
+        continue;
+
+    if (i != a)
+    {
+        Console.WriteLine("No");
+        return;
+    }
+}
+
+Console.WriteLine("Yes");
