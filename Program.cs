2f58d26bdd64b9e788a26b622c6b561c14a5a248
Ahmeddouhm <a.douha0@gmail.com>
2026-07-21 23:10:32 +0300

    Add "N - Check Code"

--- Program.cs
+++ Program.cs
@@ -3,39 +3,6 @@ int a = int.Parse(numsInput[0]);
 int b = int.Parse(numsInput[1]);
 string input = Console.ReadLine();
 
-//try
-//{
-//    bool checkLength = input.Length == a + b + 1;
-//    bool checkDash = input[a] == '-' && input.Count('-') == 1;
-//    bool checkDigits = true;
-//    string[] digitsInput = input.Split('-');
-//    string pt1 = digitsInput[0];
-//    string pt2 = digitsInput[1];
-
-//    for (int i = 0; i < pt1.Length; i++)
-//    {
-//        if (!char.IsDigit(pt1[i]))
-//        {
-//            checkDigits = false;
-//            break;
-//        }
-//    }
-//    for (int i = 0; i < pt2.Length; i++)
-//    {
-//        if (!char.IsDigit(pt2[i]))
-//        {
-//            checkDigits = false;
-//            break;
-//        }
-//    }
-
-//    Console.WriteLine(checkDigits && checkDash && checkLength ? "Yes" : "No");
-//}
-//catch
-//{
-//    Console.WriteLine("No");
-//}
-
 if (input[a] != '-')
 {
     Console.WriteLine("No");
