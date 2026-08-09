520c32fd8fe2d478d44ee5df287a7248a34a173b
Ahmeddouhm <a.douha0@gmail.com>
2026-08-09 18:07:48 +0300

    Add "Q - Count Subarrays"

--- Program.cs
+++ Program.cs
@@ -1,22 +1,27 @@
-﻿int num = int.Parse(Console.ReadLine());
-string[] input = Console.ReadLine().Split();
-double[] arr = Array.ConvertAll(input, double.Parse);
-
-int operations = 0;
-bool validOperation = true;
-
-while (validOperation)
+﻿int iterations = int.Parse(Console.ReadLine());
+while (iterations-- > 0)
 {
-	for (int i = 0; i < arr.Length; i++)
-	{
-		if (arr[i] % 2 != 0)
-		{
-			validOperation = false;
-			break;
-		}
-		arr[i] /= 2;
-	}
-	operations++;
-}
+	int len = int.Parse(Console.ReadLine());
+	
+	string[] input = Console.ReadLine().Split();
+	int[] arr = Array.ConvertAll(input, int.Parse);
 
-Console.WriteLine(operations - 1 < 0 ? 0 : operations - 1);
+	int counter = 0;
+
+	for (int i = 0; i < len; i++)
+	{
+		counter++;
+
+		for (int j = i + 1; j < len; j++)
+		{
+			if (arr[j] >= arr[j-1])
+				counter++;
+
+			else
+				break;
+		}
+	}
+
+    Console.WriteLine(counter);
+
+}
