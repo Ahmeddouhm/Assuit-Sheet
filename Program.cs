7eea75528096ac0d581f26643d5cfde372ea2163
Ahmeddouhm <a.douha0@gmail.com>
2026-08-09 21:33:26 +0300

    Add "R - Permutation with arrays"

--- Program.cs
+++ Program.cs
@@ -1,27 +1,21 @@
-﻿int iterations = int.Parse(Console.ReadLine());
-while (iterations-- > 0)
+﻿int len = int.Parse(Console.ReadLine());
+
+string[] input = Console.ReadLine().Split();
+long[] arr = Array.ConvertAll(input, long.Parse);
+
+string[] input2 = Console.ReadLine().Split();
+long[] arr2 = Array.ConvertAll(input2, long.Parse);
+
+int counter = 0;
+
+for (int i = 0; i < len; i++)
 {
-	int len = int.Parse(Console.ReadLine());
-	
-	string[] input = Console.ReadLine().Split();
-	int[] arr = Array.ConvertAll(input, int.Parse);
-
-	int counter = 0;
-
-	for (int i = 0; i < len; i++)
+	if (arr2.Contains(arr[i]))
 	{
+        int arrayIndex = Array.IndexOf(arr2, arr[i]);
+        arr2[arrayIndex] = 0;
 		counter++;
-
-		for (int j = i + 1; j < len; j++)
-		{
-			if (arr[j] >= arr[j-1])
-				counter++;
-
-			else
-				break;
-		}
 	}
-
-    Console.WriteLine(counter);
-
 }
+
+Console.WriteLine(counter == len ? "yes" : "no");
