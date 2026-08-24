bad05c5470680a4740b8199e95066cadf1263815
Ahmeddouhm <a.douha0@gmail.com>
2026-08-24 18:17:08 +0300

    Add "A - Create A New String"

--- Program.cs
+++ Program.cs
@@ -1,39 +1,5 @@
-﻿string[] input = Console.ReadLine().Split();
+﻿string input1 = Console.ReadLine();
+string input2 = Console.ReadLine();
 
-int len = int.Parse(input[0]);
-int itr = int.Parse(input[1]);
-
-string[] arrayInput = Console.ReadLine().Split();
-long[] arr = Array.ConvertAll(arrayInput, long.Parse);
-
-for (int i = 0; i < itr; i++)
-{
-    long inputLR = long.Parse(Console.ReadLine());
-
-    if (BinarySearch(arr, inputLR))
-        Console.WriteLine("found");
-	else
-        Console.WriteLine("not found");
-	
-}
-
-bool BinarySearch(long[] arr, long key) 
-{
-	long low = 0, high = arr.Length - 1;
-
-	while (low <= high)
-	{
-		long mid = low + (high - low) / 2;
-
-		if (arr[mid] == key)
-			return true;
-
-		if (arr[mid] < key)
-			low = mid + 1;
-		else
-			high = mid - 1;
-
-	}
-
-	return false;
-}
+Console.WriteLine($"{input1.Length} {input2.Length}");
+Console.WriteLine($"{input1} {input2}");
