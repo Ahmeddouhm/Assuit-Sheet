b68f387f7be56ad3c003707a33627559b7cb0d30
Ahmeddouhm <a.douha0@gmail.com>
2026-08-25 04:40:56 +0300

    Add "B - Let's use Getline"

--- Program.cs
+++ Program.cs
@@ -1,5 +1,2 @@
 ﻿string input1 = Console.ReadLine();
-string input2 = Console.ReadLine();
-
-Console.WriteLine($"{input1.Length} {input2.Length}");
-Console.WriteLine($"{input1} {input2}");
+Console.WriteLine($"{input1.Remove(input1.IndexOf('\\'))}");
