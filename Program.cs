98e7c28fbb72fd2a4975e12bc9a5e6a13cb637ca
Ahmeddouhm <a.douha0@gmail.com>
2026-08-26 04:14:17 +0300

    Add "C - Compare"

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿string input1 = Console.ReadLine();
-Console.WriteLine($"{input1.Remove(input1.IndexOf('\\'))}");
+﻿string input1 = Console.ReadLine() ?? "";
+string input2 = Console.ReadLine() ?? "";
+
+int len = Math.Min(input1.Length, input2.Length);
+
+for (int i = 0; i < len; i++)
+{
+	if (input1[i] > input2[i])
+	{
+		Console.WriteLine(input2);
+		return;
+	}
+
+	if (input1[i] < input2[i])
+	{
+		Console.WriteLine(input1);
+		return;
+	}
+}
+
+if (input1.Length > input2.Length)
+    Console.WriteLine(input2);
+else
+    Console.WriteLine(input1);
