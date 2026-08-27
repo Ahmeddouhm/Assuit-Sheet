5ae9bdf6662bac20103a76571840ccd80982d970
Ahmeddouhm <a.douha0@gmail.com>
2026-08-27 21:22:34 +0300

    Add "E - Count"

--- Program.cs
+++ Program.cs
@@ -1,23 +1,12 @@
 ﻿using System.Text;
 
 string input1 = Console.ReadLine() ?? "";
-string input2 = Console.ReadLine() ?? "";
 
-Console.WriteLine($"{input1.Length} {input2.Length}");
+long count = 0;
 
-Console.WriteLine($"{input1}{input2}");
-
-Console.WriteLine(Swap(input1, input2));
-
-string Swap(string s1, string s2) 
+for (int i = 0; i < input1.Length; i++)
 {
-    char[] chars1 = s1.ToCharArray();
-    char[] chars2 = s2.ToCharArray();
-
-    (chars1[0], chars2[0]) = (chars2[0], chars1[0]);
-
-    string output1 = new(chars1);
-    string output2 = new(chars2);
-
-    return $"{output1} {output2}";
+    count += input1[i] - '0';
 }
+
+Console.WriteLine(count);
