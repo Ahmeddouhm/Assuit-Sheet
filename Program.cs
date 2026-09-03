3235faab8e0d6d5aa7b8191a4b4b895393b80ede
Ahmeddouhm <a.douha0@gmail.com>
2026-09-03 05:16:39 +0300

    Add "K - I Love strings"

--- Program.cs
+++ Program.cs
@@ -1,18 +1,27 @@
-﻿long[] count = new long[26];
+﻿using System.Text;
 
-int c;
-while ((c = Console.Read()) != -1)
+int itrs = int.Parse(Console.ReadLine() ?? "");
+
+while (itrs-- > 0)
 {
-	if (c >= 'a' && c <= 'z')
+    string[] s1 = (Console.ReadLine() ?? "").Split();
+	string st1 = s1[0];
+	string st2 = s1[1];
+
+	int len = Math.Min(st1.Length, st2.Length);
+
+	var sb = new StringBuilder();
+
+    for (int i = 0; i < len; i++)
 	{
-		count[c - 'a']++;
+		sb.Append(st1[i]);
+		sb.Append(st2[i]);
 	}
+
+	if (st1.Length > st2.Length)
+		sb.Append(st1[len..]);
+	else
+		sb.Append(st2[len..]);
+
+    Console.WriteLine(sb.ToString());
 }
-
-for (int i = 0; i < 26; i++)
-{
-	if (count[i] > 0)
-		Console.WriteLine($"{(char)(i + 'a')} : {count[i]}");
-
-}
-
