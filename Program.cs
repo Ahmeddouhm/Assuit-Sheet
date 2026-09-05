8582729c7c7f734e7b6ce908f2d58936e6473d43
Ahmeddouhm <a.douha0@gmail.com>
2026-09-05 16:18:20 +0300

    Add "M - Subsequence String"

--- Program.cs
+++ Program.cs
@@ -1,64 +1,11 @@
-﻿string[] nums = (Console.ReadLine() ?? "").Split();
-string s = (Console.ReadLine() ?? "");
+﻿string s = (Console.ReadLine() ?? "");
+string target = "hello";
+int j = 0;
 
-int n = int.Parse(nums[0]);
-int q = int.Parse(nums[1]);
-
-for (int i = 0; i < q; i++)
+for (int i = 0; i < s.Length; i++)
 {
-    string qryInput = (Console.ReadLine() ?? "");
-    string[] qrySplitted = qryInput.Split();
-    string qry = qrySplitted[0];
-
-    switch (qry)
-    {
-        case "pop_back":
-            s = s[..^1];
-            break;
-        case "front":
-            Console.WriteLine($"{s[0]}");
-            break;
-        case "back":
-            Console.WriteLine($"{s[^1]}");
-            break;
-        case "push_back":
-            string x = qrySplitted[1];
-            s += x;
-            break;
-        case "sort":
-            int lsort = int.Parse(qrySplitted[1]);
-            int rsort = int.Parse(qrySplitted[2]);
-            if (lsort > rsort)
-            {
-                (lsort, rsort) = (rsort, lsort);
-            }
-            char[] sToArr = s[(lsort-1)..rsort].ToCharArray();
-            Array.Sort(sToArr);
-            s = s[..(lsort-1)] + new string (sToArr) + s[rsort..];
-            break;
-        case "substr":
-            int l = int.Parse(qrySplitted[1]);
-            int r = int.Parse(qrySplitted[2]);
-            if (l > r)
-            {
-                (l, r) = (r, l);
-            }
-            Console.WriteLine(s[(l-1)..r]);
-            break;
-        case "reverse":
-            int lreverse = int.Parse(qrySplitted[1]);
-            int rreverse = int.Parse(qrySplitted[2]);
-            if (lreverse > rreverse)
-            {
-                (lreverse, rreverse) = (rreverse, lreverse);
-            }
-            char[] sToArr2 = s[(lreverse-1)..rreverse].ToCharArray();
-            Array.Reverse(sToArr2);
-            s = s[..(lreverse-1)] + new string(sToArr2) + s[rreverse..];
-            break;
-        case "print":
-            int pos = int.Parse(qrySplitted[1]);
-            Console.WriteLine(s[pos-1]);
-            break;
-    }
+	if (j < target.Length && s[i] == target[j])
+		j++;
 }
+
+Console.WriteLine(j == target.Length ? "YES" : "NO");
