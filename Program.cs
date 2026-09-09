a4ca37011d0d812837c1ae805a5306049fcd9c7b
Ahmeddouhm <a.douha0@gmail.com>
2026-09-09 09:47:38 +0300

    Add " N - Max Subsequence"

--- Program.cs
+++ Program.cs
@@ -1,11 +1,22 @@
-﻿string s = (Console.ReadLine() ?? "");
-string target = "hello";
-int j = 0;
+﻿using System.Text;
 
-for (int i = 0; i < s.Length; i++)
+int itrs = int.Parse(Console.ReadLine() ?? "");
+string s = (Console.ReadLine() ?? "");
+var sb = new StringBuilder();
+
+for (int i = 0; i < itrs; i++)
 {
-	if (j < target.Length && s[i] == target[j])
-		j++;
+	if (i == 0)
+	{
+		sb.Append(s[i]);
+		continue;
+	}
+
+	char lastChar = s[i - 1];
+	if (s[i] != lastChar)
+	{
+		sb.Append(s[i]);
+	}
 }
 
-Console.WriteLine(j == target.Length ? "YES" : "NO");
+Console.WriteLine(sb.Length);
