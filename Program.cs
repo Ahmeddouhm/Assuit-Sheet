d635956c9d7e610a4f62fc6e365053255ac0576c
Ahmeddouhm <a.douha0@gmail.com>
2026-09-14 22:49:40 +0300

    Add "R - String Score"

--- Program.cs
+++ Program.cs
@@ -1,14 +1,46 @@
-﻿string[] s = (Console.ReadLine() ?? "").Split();
+﻿using System.Text;
+string num = Console.ReadLine() ?? "";
+StringBuilder s = new (Console.ReadLine() ?? "");
+int score = 0;
 
 for (int i = 0; i < s.Length; i++)
 {
-    for (int j = s[i].Length - 1; j >= 0; j--)
-    {
-        string currString = s[i];
-        Console.Write($"{currString[j]}");
-    }
-    if (i != s.Length - 1)
-    {
-        Console.Write(" ");
-    }
+	switch (s[i])
+	{
+		case 'V':
+			score += 5;
+			break;
+		case 'W':
+			score += 2;
+			break;
+		case 'X':
+			if (i < s.Length-1)
+				i++;
+			break;
+		case 'Y':
+			if (i < s.Length-1)
+			{
+				char nextChar = s[i + 1];
+				s.Append(nextChar);
+				i++;
+			}
+			break;
+		case 'Z':
+			if (i < s.Length-1)
+			{
+				if (s[i+1] == 'V')
+				{
+					score /= 5;
+					i++;
+				}
+				else if (s[i + 1] == 'W')
+				{
+					score /= 2;
+					i++;
+				}
+			}
+			break;
+	}
 }
+
+Console.WriteLine(score);
