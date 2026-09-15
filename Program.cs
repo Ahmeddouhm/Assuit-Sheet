21e57056c76d4361bc3920c50b474a5d4dc8970e
Ahmeddouhm <a.douha0@gmail.com>
2026-09-15 16:27:30 +0300

    Add "T - URL"

--- Program.cs
+++ Program.cs
@@ -1,27 +1,16 @@
-﻿using System.Text;
-string s = new (Console.ReadLine() ?? "");
-int balance = 0, start = 0;
-List<string> balancedStrings = new();
+﻿string s = new (Console.ReadLine() ?? "");
 
-for (int i = 0; i < s.Length; i++)
-{
-	if (s[i] == 'R')
-	{
-		balance++;
-	}
-	else
-	{
-		balance--;
-	}
+int questionMarkIndex = s.IndexOf('?');
+string[] parameters = s.Substring(questionMarkIndex + 1).Split('&');
 
-	if (balance == 0)
-	{
-		balancedStrings.Add(s.Substring(start, i - start + 1));
-		start = i + 1;
-	}
-}
-Console.WriteLine(balancedStrings.Count);
-foreach (var balancedStr in balancedStrings)
-{
-    Console.WriteLine(balancedStr);
-}
+string username = parameters[0].Substring(9);
+string pwd = parameters[1].Substring(4);
+string profile = parameters[2].Substring(8);
+string role = parameters[3].Substring(5);
+string key = parameters[4].Substring(4);
+
+Console.WriteLine($"username: {username}");
+Console.WriteLine($"pwd: {pwd}");
+Console.WriteLine($"profile: {profile}");
+Console.WriteLine($"role: {role}");
+Console.WriteLine($"key: {key}");
