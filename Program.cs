3b632cd57d7ad52e088164854b564b162756474e
Ahmeddouhm <a.douha0@gmail.com>
2026-09-16 02:11:51 +0300

    Add "V - Replace Word"

--- Program.cs
+++ Program.cs
@@ -1,17 +1,3 @@
-﻿string s = (Console.ReadLine() ?? "").ToLower();
-int[] frqArr = new int[5];
+﻿string s = (Console.ReadLine() ?? "");
 
-for (int i = 0; i < s.Length; i++)
-{
-	foreach (var c in s)
-	{
-		int idx = "egypt".IndexOf(c);
-
-		if (idx != -1)
-		{
-			frqArr[idx]++;
-		}
-	}
-}
-
-Console.WriteLine(frqArr.Min());
+Console.WriteLine(s.Replace("EGYPT", " "));
