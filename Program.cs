163ebd8aa181798e19178cb0af7eaa0055a5a262
Ahmeddouhm <a.douha0@gmail.com>
2026-09-16 02:34:20 +0300

    Add "V - Replace Word" V2

--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
-﻿string s = (Console.ReadLine() ?? "");
+﻿using System.Text;
 
-Console.WriteLine(s.Replace("EGYPT", " "));
+string s = (Console.ReadLine() ?? "");
+var sb = new StringBuilder();
+
+for (int i = 0; i < s.Length; i++)
+{
+	if (s[i] == 'E')
+	{
+		if (i + 5 <= s.Length && s.Substring(i, 5) == "EGYPT")
+		{
+			sb.Append(' ');
+			i += 4;
+			continue;
+		}
+	}
+	sb.Append(s[i]);
+}
+
+Console.WriteLine(sb.ToString());
