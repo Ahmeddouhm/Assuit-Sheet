6330b696703e93f089dfebd3d55ce77ca2c4dd47
Ahmeddouhm <a.douha0@gmail.com>
2026-09-16 03:11:19 +0300

    Add " W - Encrypt & Decrypt Message "

--- Program.cs
+++ Program.cs
@@ -1,20 +1,21 @@
 ﻿using System.Text;
 
-string s = (Console.ReadLine() ?? "");
+string org = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+string key = "PgEfTYaWGHjDAmxQqFLRpCJBownyUKZXkbvzIdshurMilNSVOtec#@_!=.+-*/";
+
+string option = Console.ReadLine() ?? "";
+string s = Console.ReadLine() ?? "";
+
 var sb = new StringBuilder();
+
+string from = option == "1" ? org : key;
+string to = option == "1" ? key : org;
 
 for (int i = 0; i < s.Length; i++)
 {
-	if (s[i] == 'E')
-	{
-		if (i + 5 <= s.Length && s.Substring(i, 5) == "EGYPT")
-		{
-			sb.Append(' ');
-			i += 4;
-			continue;
-		}
-	}
-	sb.Append(s[i]);
+    int idxOfS = from.IndexOf(s[i]);
+
+    sb.Append(to[idxOfS]);
 }
 
 Console.WriteLine(sb.ToString());
