0e00878f26fc5118621a8259e80471ea17a6a623
Ahmeddouhm <a.douha0@gmail.com>
2026-09-21 02:26:20 +0300

    Add "E - Swap"

--- Program.cs
+++ Program.cs
@@ -1,21 +1,6 @@
-﻿int itrs = int.Parse(Console.ReadLine() ?? "");
-
-while (itrs -- > 0)
+﻿string[] s = (Console.ReadLine() ?? "").Split();
+Console.WriteLine(Swap(s[0], s[1]));
+string Swap(string n1, string n2)
 {
-	long s = long.Parse(Console.ReadLine() ?? "");
-
-	Console.WriteLine(IsPrime(s) ? "YES" : "NO");
-}
-bool IsPrime(long num)
-{
-	if (num <= 1)
-		return false;
-
-	for (int i = 2; i*i <= num; i++)
-	{
-		if (num % i == 0)
-			return false;
-	}
-
-	return true;
+	return $"{n2} {n1}";
 }
