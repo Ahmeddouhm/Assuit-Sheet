45e56550efb90469ad7241505cfbc1e7d156a100
Ahmeddouhm <a.douha0@gmail.com>
2026-09-21 15:44:32 +0300

    Add "F - Equation"

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
-﻿string[] s = (Console.ReadLine() ?? "").Split();
-Console.WriteLine(Swap(s[0], s[1]));
-string Swap(string n1, string n2)
+﻿long[] s = Array.ConvertAll((Console.ReadLine() ?? "").Split(),long.Parse);
+Console.WriteLine(Equation(s[0], s[1]));
+long Equation(long x, long n)
 {
-	return $"{n2} {n1}";
+	long equation = 0;
+	
+	for (int i = 2; i <= n; i+=2)
+		equation += (long)Math.Pow(x, i);
+
+	return equation;
 }
