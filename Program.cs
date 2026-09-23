51420b17e3ae278e76f350ed134274f31ca01d21
Ahmeddouhm <a.douha0@gmail.com>
2026-09-23 22:26:23 +0300

    Add "J - Average"

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
-﻿string itrs = (Console.ReadLine() ?? "");
-int[] input = Array.ConvertAll((Console.ReadLine() ?? "").Split(),int.Parse);
-Max_Min(input);
-void Max_Min(int[] arr) 
+﻿int itrs = int.Parse((Console.ReadLine() ?? ""));
+double[] input = Array.ConvertAll((Console.ReadLine() ?? "").Split(), double.Parse);
+
+Console.WriteLine($"{Avg(input):F6}");
+double Avg(double[] arr) 
 {
-    Console.WriteLine($"{arr.Min()} {arr.Max()}");
+    double sum = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+        sum += arr[i];
+
+    return sum / arr.Length;
 }
