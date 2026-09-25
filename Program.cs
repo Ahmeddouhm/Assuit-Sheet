97d21a4ffbc29fbc654bba294959938ac90a1064
Ahmeddouhm <a.douha0@gmail.com>
2026-09-25 03:27:51 +0300

    Add "K - Shift Right"

--- Program.cs
+++ Program.cs
@@ -1,41 +1,23 @@
 ﻿int[] input = Array.ConvertAll((Console.ReadLine() ?? "").Split(), int.Parse);
-int n = input[0], x = --input[1], y = --input[2];
-int[,] twoDimArray = new int[n, n];
+int n = input[0], x = input[1];
+int[] array = Array.ConvertAll((Console.ReadLine() ?? "").Split(), int.Parse);
+List<int> ints = new List<int>(array);
 
-for (int i = 0; i < n; i++)
+int shiftTimes = x % n;
+
+Shift_Right_Array(array, shiftTimes);
+Print_Array(ints);
+
+void Shift_Right_Array(int[] array, int shifts)
 {
-    int[] row = Array.ConvertAll((Console.ReadLine() ?? "").Split(), int.Parse);
-
-    for (int j = 0; j < n; j++)
-        twoDimArray[i, j] = row[j];
-}
-
-Swap_Rows_2D_Array(twoDimArray, x, y);
-Swap_Cols_2D_Array(twoDimArray, x, y);
-Print_2D_Array(twoDimArray);
-
-void Swap_Rows_2D_Array(int[,] twoDimArray, int r1, int r2)
-{
-    for (int i = 0; i < twoDimArray.GetLength(1); i++)
+    for (int i = 0; i < shifts; i++)
     {
-        (twoDimArray[r1,i], twoDimArray[r2,i]) = (twoDimArray[r2,i], twoDimArray[r1,i]);
+        ints.Insert(0,ints[ints.Count - 1]);
+        ints.RemoveAt(ints.Count - 1);
     }
 }
-void Swap_Cols_2D_Array(int[,] twoDimArray, int c1, int c2)
+void Print_Array(List<int> array) 
 {
-    for (int i = 0; i < twoDimArray.GetLength(0); i++)
-    {
-        (twoDimArray[i,c1], twoDimArray[i,c2]) = (twoDimArray[i,c2], twoDimArray[i,c1]);
-    }
-}
-void Print_2D_Array(int[,] twoDimArray) 
-{
-    for (int i = 0; i < twoDimArray.GetLength(0); i++)
-    {
-        for (int j = 0; j < twoDimArray.GetLength(1); j++)
-        {
-            Console.Write($"{twoDimArray[i, j]} ");
-        }
-        Console.WriteLine();
-    }
+    for (int i = 0; i < array.Count; i++)
+        Console.Write($"{array[i]} ");
 }
